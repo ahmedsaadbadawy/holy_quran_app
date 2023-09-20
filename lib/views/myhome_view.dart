059d8cf1c2d9bd07a0090c6833f237c19715c261
ahmedsaadbadawy <ahmedzack168@gmail.com>
2023-09-20 21:54:43 +0300

    first commit

---
+++ lib/views/myhome_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyHomeView extends StatelessWidget {
+  const MyHomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
